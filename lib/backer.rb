c468cc00b1fe8f09da9ab90a0f63fa9b9aa1f4ae
Kayla Cross <kac288@georgetown.edu>
2018-03-30 17:20:23 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,9 +9,13 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
+<<<<<<< HEAD
     if !project.backers.include?(self)
       project.add_backer(self)
     end
+=======
+    project.add_backer(self)
+>>>>>>> b81a1589038fc75c4b3020e54a94d2dc3c8ed2fe
   end
   
 end
--- lib/project.rb
+++ lib/project.rb
@@ -9,9 +9,13 @@ class Project
   
   def add_backer(backer)
     @backers << backer
+<<<<<<< HEAD
     if !backer.backed_projects.include?(self)
       backer.back_project(self)
     end
+=======
+    backer.back_project(self)
+>>>>>>> b81a1589038fc75c4b3020e54a94d2dc3c8ed2fe
   end
   
 end
